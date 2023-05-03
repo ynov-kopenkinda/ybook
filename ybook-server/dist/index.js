4734c675121f8af771d4c541e72812c0717e2f6a
Dmitrii <kopenkin.da@gmail.com>
2023-05-03 18:40:35 +0200

    Remove built app

--- ybook-server/dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const server_1 = require("./server");
-(0, server_1.startServer)();
-process.on("uncaughtException", (error) => {
-    console.error("CRITICAL: UNCAUGHT ERROR");
-    console.error(error);
-});
-//# sourceMappingURL=index.js.map
